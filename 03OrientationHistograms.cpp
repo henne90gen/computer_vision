564fa3bf778084856bc49f32f1b1105f4fbbec95
Henne <henne90gen@gmail.com>
2018-11-06 17:30:52 +0100

    It finally works!

--- 03OrientationHistograms.cpp
+++ 03OrientationHistograms.cpp
@@ -18,61 +18,62 @@ int main(int argc, char **argv) {
     cv::Mat gradient_ver;
     cv::Sobel(original, gradient_hor, CV_32F, 1, 0, 1);
     cv::Sobel(original, gradient_ver, CV_32F, 0, 1, 1);
-    gradient_hor = (gradient_hor + 255.0) / 2.0;
-    gradient_ver = (gradient_ver + 255.0) / 2.0;
-    gradient_hor.convertTo(gradient_hor, CV_8U);
-    gradient_ver.convertTo(gradient_ver, CV_8U);
+    cv::Mat show_gradient_hor = (gradient_hor + 255.0) / 2.0;
+    cv::Mat show_gradient_ver = (gradient_ver + 255.0) / 2.0;
+    show_gradient_hor.convertTo(show_gradient_hor, CV_8U);
+    show_gradient_ver.convertTo(show_gradient_ver, CV_8U);
 
-    cv::Mat gradient_angles = gradient_hor.clone();
+    imshow("gradient-hor", show_gradient_hor);
+    imshow("gradient-ver", show_gradient_ver);
+
+    cv::Mat gradient_angles = cv::Mat::zeros(gradient_hor.rows, gradient_hor.cols, CV_8U);
     cv::Mat gradient_magnitudes = gradient_hor.clone();
-    cv::Mat histogram = cv::Mat::zeros(1, 36, CV_32F);
+    int num_hist_bins = 360;
+    double histogram[360] = {0};
     for (int row = 0; row < gradient_ver.rows; row++) {
-        uchar *p_ver = gradient_ver.ptr(row);
-        uchar *p_hor = gradient_hor.ptr(row);
+        float *p_ver = gradient_ver.ptr<float>(row);
+        float *p_hor = gradient_hor.ptr<float>(row);
         uchar *p_ang = gradient_angles.ptr(row);
-        uchar *p_mag = gradient_magnitudes.ptr(row);
+        float *p_mag = gradient_magnitudes.ptr<float>(row);
         for (int col = 0; col < gradient_ver.cols; col++) {
             double x = (double)*p_hor;
             double y = (double)*p_ver;
-            if (x != 0 || y != 0) {
-                std::cout << "(" << x << "," << y << ") ";
-            }
-            double angle = (atan2(y, x) + M_PI) / M_PI * 180.0;
-            angle -= 180.0;
+            double magnitude = sqrt(x * x + y * y);
+            double angle = atan2(-y, x) / M_PI * 180.0;
             if (angle < 0) {
                 angle += 360.0;
             }
-            double magnitude = abs(sqrt(y * y + x * x));
-            *p_mag = (uchar)magnitude;
-            uchar angle_uchar = (uchar)(angle / 360.0 * 255.0);
-            *p_ang = angle_uchar;
-            histogram.at<float>(angle / 10.0) += magnitude;
+            if (magnitude > 30) {
+                histogram[(int)angle] += magnitude;
+            }
+            *p_mag = magnitude;
+            *p_ang = (uchar)(angle / 360.0 * 255.0);
             p_ver++;
             p_hor++;
             p_ang++;
+            p_mag++;
         }
-        std::cout << std::endl;
     }
 
     double x1, x2, x3, y1, y2, y3, prev_x, prev_y;
     bool lookingForNext = false;
     double max = 0.0;
-    for (int i = 0; i < histogram.cols; i++) {
-        if (max < histogram.at<float>(i)) {
-            max = histogram.at<float>(i);
+    for (int i = 0; i < 360; i++) {
+        if (max < histogram[i]) {
+            max = histogram[i];
         }
-        if (y2 < histogram.at<float>(i)) {
-            y2 = histogram.at<float>(i);
+        if (y2 < histogram[i]) {
+            y2 = histogram[i];
             x2 = i;
             y1 = prev_y;
             x1 = prev_x;
             lookingForNext = true;
         }
-        if (histogram.at<float>(i) != 0) {
-            prev_y = histogram.at<float>(i);
+        if (histogram[i] != 0) {
+            prev_y = histogram[i];
             prev_x = i;
-            if (lookingForNext && histogram.at<float>(i) != y2) {
-                y3 = histogram.at<float>(i);
+            if (lookingForNext && histogram[i] != y2) {
+                y3 = histogram[i];
                 x3 = i;
                 lookingForNext = false;
             }
@@ -97,12 +98,11 @@ int main(int argc, char **argv) {
     cv::invert(A, A);
     cv::Mat result = A * B;
 
-    cv::Mat hist = cv::Mat::zeros(255, 255, CV_8UC3);
+    cv::Mat hist = cv::Mat::zeros(360, 360, CV_8UC3);
     for (int x = 0; x < hist.cols; x++) {
-        double lookup = (double)x / 255.0 * 36.0;
-        double y = histogram.at<float>((int)lookup);
-        y = y / max * 255.0;
-        line(hist, cv::Point(x, 255), cv::Point(x, 255.0 - y), CV_RGB(255, 255, 255));
+        double y = histogram[x];
+        y = y / max * 360.0;
+        line(hist, cv::Point(x, 360), cv::Point(x, 360.0 - y), CV_RGB(255, 255, 255));
     }
     imshow("hist", hist);
 
@@ -111,6 +111,7 @@ int main(int argc, char **argv) {
     double c = result.at<float>(2);
 
     double mainAngle = (-1.0 * b) / (2.0 * a);
+    std::cout << mainAngle << std::endl;
 
     cv::Point2f original_center(original.cols / 2.0, original.rows / 2.0);
     cv::Mat rot_mat = cv::getRotationMatrix2D(original_center, -1.0 * mainAngle, 1.0);
@@ -118,12 +119,19 @@ int main(int argc, char **argv) {
     cv::warpAffine(original, rotated, rot_mat, original.size());
     imshow("original", original);
     imshow("rotated", rotated);
+
+    cv::Mat show_gradient_angles;
+    gradient_magnitudes.convertTo(show_gradient_angles, CV_8U, 0.71);
     imshow("gradient-angles", gradient_angles);
-    imshow("gradient-magnitudes", gradient_magnitudes);
+
+    cv::Mat show_gradient_magnitudes;
+    gradient_magnitudes.convertTo(show_gradient_magnitudes, CV_8U, 0.71);
+    imshow("gradient-magnitudes", show_gradient_magnitudes);
 
     while (true) {
-        if (cv::waitKey(0) == 27)
+        if (cv::waitKey(0) == 27) {
             break;
+        }
     }
 
     return 0;
